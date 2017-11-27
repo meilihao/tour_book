6dcb0661e5e126fe66f3c73af54c229b67fea229
chenhao <563278383@qq.com>
2017-11-27 18:09:59 +0800

    u 20171127

--- docker/dockerfile/jaeger_elasticsearch/start_jaeger.sh
+++ docker/dockerfile/jaeger_elasticsearch/start_jaeger.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # env
-ESServerUrls=${ESServerUrls:-"/usr/bin/cqlsh"}
+ESServerUrls=${ESServerUrls:-"http://localhost:9200"}
 
 # start jaeger
-/jaeger/cmd/collector 2>&1 > collector.log --span-storage.type=cassandra --es.server-urls="${ESServerUrls}" &
-/jaeger/cmd/agent 2>&1 > agent.log --http-server.host-port=:6831 --collector.host-port=127.0.0.1:14267 &
-/jaeger/cmd/query 2>&1 > query.log --query.static-files=/jaeger/jaeger-ui/build/ --span-storage.type=cassandra --cassandra.keyspace="${KEYSPACE}"
+cmd/collector 2>&1 > collector.log --span-storage.type=elasticsearch --es.server-urls="${ESServerUrls}" &
+cmd/agent 2>&1 > agent.log --http-server.host-port=:6831 --collector.host-port=127.0.0.1:14267 &
+cmd/query 2>&1 > query.log --query.static-files=jaeger-ui/build/ --span-storage.type=elasticsearch --es.server-urls="${ESServerUrls}" &
