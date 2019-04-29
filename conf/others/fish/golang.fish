89a8ca5d5dd67eb6d6620c638f79d16c9ad322d9
meilihao <563278383@qq.com>
2019-04-29 18:26:37 +0800

    + golang.fish

---
+++ conf/others/fish/golang.fish
@@ -0,0 +1,9 @@
+set -gx GOROOT /opt/go
+set -gx GOPATH /home/chen/git/go
+set -gx PATH $GOROOT/bin $GOPATH/bin $PATH
+
+## /etc/profile
+## golang
+#export GOROOT=/opt/go
+#export GOPATH=/home/chen/git/go
+#export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
